e093de428367a65b26e263af29c8eb6f5667dea3
Gustavo Melo <guto.smelo@gmail.com>
2024-01-06 17:31:29 -0300

    function generica

--- generator.js
+++ generator.js
@@ -1,7 +1,19 @@
-function gerarQR(e){
-	//console.log(e)
+(function(){
+	var e = document.querySelector('#guide_qrcode');
 	e.removeAttribute('onload');
+
+	var size = e.getAttribute('size');
+	var getCookieValue = function (name){
+	  return document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
+	}
+
+	var cookieTeste = getCookieValue('teste');
+
+	if(size == null){
+		size = "100x100";
+	}
+
 	var deeplink = "https://deeplink.guideinvestimentos.com.br/onboarding";
-	var urlapi = "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data="+deeplink;
+	var urlapi = "https://api.qrserver.com/v1/create-qr-code/?size="+size+"&data="+deeplink+"?"+cookieTeste;
 	e.setAttribute('src', urlapi);
-}
+})();
