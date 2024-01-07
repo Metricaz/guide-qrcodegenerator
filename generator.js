7d26612c940421db45e4fd6c65def6e10628e645
Gustavo Melo <guto.smelo@gmail.com>
2024-01-07 15:19:36 -0300

    inclusao do cookie source e elemento sem id

--- generator.js
+++ generator.js
@@ -1,19 +1,25 @@
 (function(){
-	var e = document.querySelector('#guide_qrcode');
+	var e = document.querySelector('img[src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="]');
 	e.removeAttribute('onload');
 
-	var size = e.getAttribute('size');
 	var getCookieValue = function (name){
 	  return document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
 	}
 
-	var cookieTeste = getCookieValue('teste');
+	var utms = getCookieValue('origemMTZ');
+	var parameters = '';
 
+	if(utms.length > 0){
+		var utm_decoded = JSON.parse(decodeURIComponent(utms));
+		parameters = '?utm_source='+utm_decoded.utm_source;
+	}
+
+	var size = e.getAttribute('size');
 	if(size == null){
 		size = "100x100";
 	}
 
 	var deeplink = "https://deeplink.guideinvestimentos.com.br/onboarding";
-	var urlapi = "https://api.qrserver.com/v1/create-qr-code/?size="+size+"&data="+deeplink+"?"+cookieTeste;
+	var urlapi = "https://api.qrserver.com/v1/create-qr-code/?size="+size+"&data="+deeplink+parameters;
 	e.setAttribute('src', urlapi);
 })();
